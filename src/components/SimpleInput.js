e496cd8344e2ebe993d639bb22f6a4167b33d1fe
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-01-30 10:04:15 +0100

    Getting value with state and by useRef.

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,35 @@
+import { useRef, useState } from 'react';
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+    console.log('Entered name: ' + enteredName);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    console.log('Entered name: ' + enteredName); // Conventional way using useState
+
+    const enteredValue = nameInputRef.current.value; // Alternative way using useRef
+    console.log('Entered ref value: ' + enteredValue);
+
+    setEnteredName('');
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          value={enteredName}
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
